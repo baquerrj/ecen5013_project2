c4021da7a580bee77607e93dc9d60895da21e8fa
Roberto Baquerizo <roba8460@colorado.edu>
2019-04-20 22:24:22 -0600

    sleep for msec or usecs

---
+++ include/delay.h
@@ -0,0 +1,29 @@
+/*!
+ * @file	delay.h
+ *
+ * @brief
+ *
+ *  Created on: Apr 20, 2019
+ *      Author: Roberto Baquerizo
+ */
+
+#ifndef _DELAY_H_
+#define _DELAY_H_
+
+
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+
+extern uint32_t g_sysClock;
+
+static inline void delayMs( uint32_t ms )
+{
+    MAP_SysCtlDelay( (g_sysClock/(1000 *3)) * ms );
+}
+
+static inline void delayUs( uint32_t us)
+{
+    MAP_SysCtlDelay( (g_sysClock/(1000000 * 3)) *us );
+}
+
+#endif /* _DELAY_H_ */
