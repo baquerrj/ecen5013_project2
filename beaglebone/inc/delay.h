90ba2723f96e693b430ed4a05171fa210e09df62
Roberto Baquerizo <roba8460@colorado.edu>
2019-04-29 19:24:19 -0600

    added a timer

--- beaglebone/inc/delay.h
+++ beaglebone/inc/delay.h
@@ -22,8 +22,10 @@
 
 #include <stdint.h>
 #include <unistd.h>
+#include <sys/time.h>
 
-
+static struct timeval start, end;
+static long mtime, seconds, useconds;
 static inline void delayMs( uint32_t ms )
 {
     usleep( 1000 * ms );
@@ -34,4 +36,18 @@ static inline void delayUs( uint32_t us )
     usleep( us );
 }
 
+static inline void __start_timer( void )
+{
+    gettimeofday( &start, NULL );
+}
+
+static inline long __millis( void )
+{
+    gettimeofday( &end, NULL );
+    seconds = end.tv_sec - start.tv_sec;
+    useconds = end.tv_usec - start.tv_usec;
+
+    mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
+    return mtime;
+}
 #endif   /* _DELAY_H_ */
